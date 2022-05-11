9003a1ab8efa59187f5bbcbe30fe817868f16496
calistus-igwilo <ebunilo@yahoo.com>
2022-05-11 05:49:29 +0100

    wordcount

--- calendar.sh
+++ calendar.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# script to print the calendar of an entire year
 echo -n "Please enter a year: "
 read year
 echo "Calendar of $year"
---
+++ word-count.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Count the number of lines in a file
+# Other arguments could be: 
+# -w for number of words, 
+# -m for no. of characters
+
+nlines=$(wc -l < $1)
+echo "There are $nlines lines in $1"
