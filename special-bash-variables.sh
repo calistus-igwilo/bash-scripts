05b1bfc46c937f99e513c275a517f4199a840697
calistus-igwilo <ebunilo@yahoo.com>
2022-05-12 05:38:41 +0100

    Bash special variables

---
+++ special-bash-variables.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "The name of the script: $0"
+echo "The number of arguments passed to the script: $#"
+echo "The values of all arguments passed to the script: $@"
+echo "The process ID of current shell: $$"
