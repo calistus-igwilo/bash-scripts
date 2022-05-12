c62f1854cd1b6e5466db899e072fdd02bbe54394
calistus-igwilo <ebunilo@yahoo.com>
2022-05-12 13:58:30 +0100

    add giga2mega

--- giga2mega.sh
+++ giga2mega.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Convert Giga to Mega
-# Pass the value to convert as an argument 
+# the value to be converted must be passed to the script as argument 
 
 GIGA=$1
-MEGA=$(($GIGA * 1024))
+MEGA=$(($GIGA*1024))
 
 echo "$GIGA GB is equal to $MEGA MB"
