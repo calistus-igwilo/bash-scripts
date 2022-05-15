aaea2f0acb5912a30d040f08ef5b6da2d4972119
calistus-igwilo <ebunilo@yahoo.com>
2022-05-15 12:54:02 +0100

    create-users

--- create-users.sh
+++ create-users.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 # Create users from a list of names stored in CSV file
-# Add each user to "developers" group which was created prior to running the script
+# Add each user to "developers" group
 # Create .ssh directory for each user and add public key to authorized_keys
 
 names=$(cat inventory.csv)
+
+# create "developers" group
+sudo groupadd developers &>/dev/null
+if [ $? -eq 0 ]; then 
+    echo "developers group created"
+else 
+    echo "developers group already exists"
+fi
 
 for i in $names; do
     # create user if it does not exist
@@ -17,12 +25,13 @@ for i in $names; do
     fi
 
     # add authorized keys
-    sudo egrep "^$i" /etc/passwd &>/dev/null
+    # sudo egrep "^$i" /etc/passwd &>/dev/null
+    sudo ls /home/$i/.ssh &>/dev/null
     if [ $? -eq 0 ]; then
         echo "$i's .ssh directory already exists"
     else 
         sudo mkdir /home/$i/.ssh &>/dev/null
-        #sudo cp authorized_keys /home/$i/.ssh/ &>/dev/null
+        sudo cp authorized_keys /home/$i/.ssh/ &>/dev/null
         echo "..ssh directory and keys added for $i"
     fi
-done 
+done
