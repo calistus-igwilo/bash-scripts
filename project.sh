e0bbf7c108c99c279cbd3e1cf872e0b4d55bb8d0
calistus-igwilo <ebunilo@yahoo.com>
2022-06-11 13:38:38 +0100

    add project.sh file

---
+++ project.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Program to output a system information page
+echo "<html>
+    title="System Information Report"
+     <head>
+          <title>$title</title>
+     </head>
+     <body>
+          <h1>$title</h1>
+     </body>
+</html>"
