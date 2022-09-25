217ed8959c9723896edd1afce14f9d94d5f35cab
calistus-igwilo <ebunilo@yahoo.com>
2022-09-26 00:17:23 +0100

    Holes in array

--- array.sh
+++ array.sh
@@ -27,4 +27,43 @@ echo ${files[*]}
 unset files
 echo "array deleted ${files[*]}"
 
+# get all the values of BASH_VERSINFO array
+./extension.sh "${BASH_VERSINFO[*]}"
+# 3 2 57 1 release x86_64-apple-darwin21
+
+# get the length of element in the 5th index
+./extension.sh "${BASH_VERSINFO[5]}"
+# :21:
+
+# get the 2nd and 3rd elements in BASH_VERSINFO array
+echo ${BASH_VERSINFO[@]:1:2}
+# 2 57
+
+# Holes in Indexed Array
+array=( a b c d e f g h i j )
+unset array[2] array[4] array[6] array[8]
+./extension.sh "${array[@]}"
+# :a:
+# :b:
+# :d:
+# :f:
+# :h:
+# :j:
+
+# With a sparse (or any) array, the ${!array[@]} expansion lists the subscripts:
+./extension.sh "${!array[@]}"
+# :0:
+# :1:
+# :3:
+# :5:
+# :7:
+# :9:
+
+# to remove the holes
+array=( "${array[@]}" )     # converts associative array into an indexed array
+echo "${!array[@]}"
+# 0 1 2 3 4 5 
+
+
+
 
